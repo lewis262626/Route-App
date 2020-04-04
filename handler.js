ac116391c49eddb20509dce1dcdfa76633303fdc
Lewis Robbins <lewis.robbins2@gmail.com>
2020-04-04 20:10:20 +0100

    backend finished

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 import * as dynamoDbLib from "./libs/dynamo-lib";
 import { success, failure } from "./libs/response-lib";
+import { getDistance } from 'geolib';
 
 export const hello = async (event, context) => {
   const data = JSON.parse(event.body);
@@ -7,20 +8,54 @@ export const hello = async (event, context) => {
   const params = {
     TableName: process.env.tableName,
 
-    Key: {
-      icao: data.icao,
+    FilterExpression: "runway_length >= :max_length and type_airport = :type and country = :country",
+    ExpressionAttributeValues: {
+      ":max_length": 7500,
+      ":type": "Airports",
+      ":country": data.country,
     }
   };
 
+  let airports;
   try {
-    const results = await dynamoDbLib.call("get", params);
+    const results = await dynamoDbLib.call("scan", params);
 
-    if (results.Item) {
-      return success(results.Item);
+    if (results.Items) {
+      airports = results.Items;
     } else {
-      return failure({ status: false, error: "item not found"});
+      return failure({ status: false, error: "No airports found" });
     }
   } catch (e) {
-    return failure({ status: false});
+    console.log(e);
+    return failure({ status: false });
   }
+
+  let i, j;
+  let lst = [];
+  for (i = 0; i < airports.length; i++) {
+    for (j = 0; j < airports.length; j++) {
+      let distance = getDistance(
+        { latitude: parseFloat(airports[i].lat), longitude: parseFloat(airports[i].lon) },
+        { latitude: parseFloat(airports[j].lat), longitude: parseFloat(airports[j].lon) }
+      );
+
+      if (distance/1000 > 100 && distance/1000 < 300) {
+        let pair = { airport1: i, airport2: j, distance: distance };
+        lst.push(pair);
+      }
+    }
+  }
+
+  if (lst.length == 0) {
+    return failure({ status: false, error: "No airpots found" });
+  }
+
+  let randomNum = Math.floor(Math.random() * lst.length);
+  let airport_dic = lst[randomNum];
+  let resp = {
+    'distance': airport_dic.distance / 1000,
+    'airport1': airports[airport_dic.airport1],
+    'airport2': airports[airport_dic.airport2]
+  };
+  return success(resp);
 };
--- libs/response-lib.js
+++ libs/response-lib.js
@@ -10,7 +10,6 @@ function buildResponse(statusCode, body) {
         statusCode: statusCode,
         headers: {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
         },
         body: JSON.stringify(body)
     };
